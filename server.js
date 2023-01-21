c430ef1142f4667a5d52b9db52702b7f97cd4e8b
Genius <genius21513@gmail.com>
2023-01-21 16:14:54 +0900

    Complete ts-auth server

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ var corsOptions = {
   origin: [
     // "http://localhost:4000",
 
-    "http://localhost",
-    "http://localhost:3000",
+    // "http://localhost",
+    // "http://localhost:3000",
     
-    "http://13.53.64.138",
-    "http://13.53.64.138:3000",
+    // "http://13.53.64.138",
+    // "http://13.53.64.138:3000",
 
     // // "http://10.10.13.227",
     // "http://10.10.13.227:3000",
