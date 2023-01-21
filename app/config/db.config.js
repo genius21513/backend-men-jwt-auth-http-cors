127baab942b53e83b6ee6e4292f018a986d8b1f4
Genius <genius21513@gmail.com>
2023-01-21 14:07:17 +0900

    Set as aws mongodb

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -6,9 +6,10 @@ module.exports = {
 
   /** Remote Host AWS */
   // #################### Connstring: "mongodb://mongdbuser:asdasd@13.53.64.138:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2",
-  // Connstring: "mongodb://mongdbuser:asdasd@13.53.64.138:27017/bezkoder_db?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2&authSource=admin",  
+  Connstring: "mongodb://mongdbuser:asdasd@13.53.64.138:27017/bezkoder_db?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2&authSource=admin",  
+  // Connstring: "mongodb://mongdbuser:asdasd@localhost:27017/bezkoder_db?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2&authSource=admin",  
 
   /** Remote Host Atlas */
   // ##################### Connstring: "mongodb+srv://jenistian:MLfqpRcZe4K0qKJb@cluster0.uhizukh.mongodb.net/?retryWrites=true&w=majority",
-  Connstring: "mongodb+srv://jenistian:MLfqpRcZe4K0qKJb@cluster0.uhizukh.mongodb.net/bezkoder_db?retryWrites=true&w=majority",  
+  // Connstring: "mongodb+srv://jenistian:MLfqpRcZe4K0qKJb@cluster0.uhizukh.mongodb.net/bezkoder_db?retryWrites=true&w=majority",  
 };
