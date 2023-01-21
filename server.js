c775a4f90e9fdfaf3d48a8859385abbccbe5cd7a
Genius <genius21513@gmail.com>
2023-01-21 13:53:07 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,11 @@ var corsOptions = {
     // "http://localhost:4000",
 
     "http://localhost",
-    // "http://localhost:3000",
+    "http://localhost:3000",
     
+    "http://13.53.64.138",
+    "http://13.53.64.138:3000",
+
     // // "http://10.10.13.227",
     // "http://10.10.13.227:3000",
 
